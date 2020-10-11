039834a9dd5866470fd2ac26da199decef8ff59f
Carlareneedc <carla.renee.diazc@gmail.com>
2020-10-11 14:02:07 -0300

    arreglando vista japan

--- src/Components/JapanQueen.jsx
+++ src/Components/JapanQueen.jsx
@@ -8,22 +8,20 @@ import {
   MDBNavItem,
   MDBNavLink,
   MDBView,
- 
 } from "mdbreact";
-import { BrowserRouter as Router, Link} from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import "../styleCSS/NavBar.css";
-import FooterPage from "../Components/Footer"
+import FooterPage from "../Components/Footer";
 import logo from "../img/logo.png";
 import contacto from "../img/contacto.png";
 import japanQueen from "../img/japanqueennores.png";
 import japan from "../img/proyectouno.png";
 import gav from "../img/gav.png";
 import unimarc from "../img/unimarc.png";
-import "../styleCSS/JapanQueen.css"
-
+import "../styleCSS/JapanQueen.css";
 
 class JapanQueen extends React.Component {
   constructor(props) {
@@ -49,7 +47,7 @@ class JapanQueen extends React.Component {
                 <img className="logo" src={logo} alt="imglogo" />
               </MDBNavbarBrand>
               {!this.state.isWideEnough && (
-                <MDBNavbarToggler className="burger"  onClick={this.onClick} />
+                <MDBNavbarToggler className="burger" onClick={this.onClick} />
               )}
               <MDBCollapse isOpen={this.state.collapse} navbar>
                 <MDBNavbarNav
@@ -88,51 +86,69 @@ class JapanQueen extends React.Component {
                     </MDBNavLink>
                   </MDBNavItem>
                   <MDBNavbarBrand href="/">
-                    <a href="mailto:carla.renee.diazc@gmail.com"><img className="" src={contacto} alt="imgcontacto" /></a>
+                    <a href="mailto:carla.renee.diazc@gmail.com">
+                      <img className="" src={contacto} alt="imgcontacto" />
+                    </a>
                   </MDBNavbarBrand>
                 </MDBNavbarNav>
               </MDBCollapse>
             </MDBNavbar>
           </Router>
-          
-          <div className="flex-center ctnJapan">
-              <div className="row">
-                <div className="col-sm-12 col-md-5 ">
-                  <img className="img-fluid japanQueen" src={japanQueen} alt="imgjapan" />
-                </div>
 
-                <div className="textContJapan col-sm-12 col-md-7 ">
-                  <h5 className="textJapan col-sm-12">
-                    <mark style={{
-              background: "rgba(220, 21, 21, 0.23)",
-              color: "rgba(89, 89, 89, 1)",
-            }}
-          >Japan Queen</mark>
-                 
-                  </h5>
-                  
-                  <p className=" col-sm-12  japanEs">
-                  Japan Queen es una app web diseñada para restaurantes, que se origina como solución a la gestión de pedidos por medio de una interfaz simple y amigable, evitando el antiguo uso de comandas y toma de ordenes a mano.
-                  </p>
+          <div className="ctnJapan">
+            <div className="row">
+              <div className="col-sm-12 col-md-5 ">
+                <img
+                  className="img-fluid japanQueen"
+                  src={japanQueen}
+                  alt="imgjapan"
+                />
+              </div>
+
+              <div className="textContJapan col-sm-12 col-md-7 ">
+                <h5 className="textJapan col-sm-12">
+                  <mark
+                    style={{
+                      background: "rgba(220, 21, 21, 0.23)",
+                      color: "rgba(89, 89, 89, 1)",
+                    }}
+                  >
+                    Japan Queen
+                  </mark>
+                </h5>
+
+                <p className=" col-sm-12 japanEs">
+                  Japan Queen es una app web diseñada para restaurantes, que se
+                  origina como solución a la gestión de pedidos por medio de una
+                  interfaz simple y amigable, evitando el antiguo uso de
+                  comandas y toma de ordenes a mano.
+                </p>
                 <div className="">
-                  <div className="col-sm-12 col-md-6  float-left" > <h5 className="rolText " >Rol</h5><p className="ux">UX/UI Designer</p></div>
-                <div className=" col-sm-12 col-md-6 float-right" ><h5 className="rolText ">Fecha del Proyecto</h5><p className="julio">Julio 2020</p></div>
-                
-                </div>
-                
+                  <div className=" col-sm-12 col-md-6  float-left">
+                    {" "}
+                    <h5 className="rolText ">Rol</h5>
+                    <p className="ux">UX/UI Designer</p>{" "}
+                    <h5 className=" rolText">Actividades</h5>
+                    <div className="row col-md-12">
+                    <p className="uxR">UX Research</p>
+                    <p className="ml-3 proto">Prototipo</p>
+                    <p className="ui">Diseño UI</p>
+                    </div>
+                  </div >
+                  <div className=" col-sm-12 col-md-6 float-right">
+                    <h5 className="rolText ">Fecha del Proyecto</h5>
+                    <p className="julio">Julio 2020</p>
+                   
+                  </div>
+                  
                 </div>
                 
               </div>
-             
             </div>
-      
-          
-                 
-                 
-                  
-          </header>
           </div>
-              );
-            }
-          }
-          export default JapanQueen;
+        </header>
+      </div>
+    );
+  }
+}
+export default JapanQueen;
