c5d075b13a9d55a84f1e0c536e5d035f47a82572
Carlareneedc <carla.renee.diazc@gmail.com>
2020-10-17 19:20:16 -0300

    terminando vista gav

--- src/Components/Gav.jsx
+++ src/Components/Gav.jsx
@@ -299,7 +299,7 @@ class JapanQueen extends React.Component {
                   El contexto de la pandemia generó cambios en los hábitos de
                   todas las personas en el mundo, pero aún más en la vida de los
                   niños. Es difícil para ellos llegar a entender las
-                  implicancias de la pandemia considerando que son graves y no
+                  sus consecuencias, considerando que son graves y no
                   se deben tomar a la ligera. El desafío en este caso es
                   explicar por ejemplo, por qué no pueden abrazar a sus amigos,
                   ir al colegio o ver a sus familiares.
@@ -308,7 +308,7 @@ class JapanQueen extends React.Component {
                   Por otro lado considerar que en base al rango etario de estos
                   niños, hay muchos de ellos que no saben leer ni escribir, por
                   ende no podrán comprender información escrita. Así mismo poder
-                  considerar a personas con capacidades diferentes y también
+                 incluir a personas con capacidades diferentes y también
                   dificultades para acceder a WiFi.
                 </p>
               </div>
@@ -327,7 +327,7 @@ class JapanQueen extends React.Component {
                     Procesos
                   </mark>
                 </h5>
-                <p className="textoProcesos">
+                <p className="textoProcesosgav">
                   En base al contexto de pandemia, realicé una encuesta{" "}
                   <strong>
                     enfocada a padres o tutores de niños entre 4 a 9 años
@@ -335,23 +335,45 @@ class JapanQueen extends React.Component {
                   , rango etario que se tomó como hallazgo dentro de la
                   investigación. Si bien, nuestros usuario directos son los
                   padres o tutores, el objetivo principal es que ellos puedan
-                  guiar a los niños en el uso de la página. El desafío en base a a los usuarios, también se relaciona con la posibilidad de darle solución al problema planteado anteriormente y poder incorporar la inclusión. Para esto se definieron usuarios indirectos, los que no acceden por si mismos a la página, sin embargo pueden conocer su contenido.
+                  guiar a los niños en el uso de la página. 
                 </p>
+                <p className="eldesafio">El desafío en base a a los usuarios, también se relaciona con la posibilidad de darle solución al problema planteado anteriormente y poder incorporar la inclusión. Para esto se definieron usuarios indirectos, los que no acceden por si mismos a la página, sin embargo pueden conocer su contenido.</p>
                 <p className="textopro">
-                  <strong>Personas con capacidades diferentes</strong> En este caso, se toma en cuenta a personas que tengan algún tipo de discapacidad visual como daltonismo.
+                  <strong>Personas con capacidades diferentes.</strong> En este caso, se consideran a personas que tengan algún tipo de discapacidad visual como daltonismo o disminución de la visión.
                 </p>
                 <p className="mesero">
-                  <strong>Niños que no saben leer ni escribir</strong> Accederá a la plataforma para generar
-                  el pedido del cliente
+                  <strong>Niños que no saben leer ni escribir.</strong> serán guiados por sus padres en el acceso al contenido.
                 </p>
-                <p className="chef">
-                  <strong>Chef </strong> Recibirá el pedido
-                </p>
+               
               </div>
               <div className=" col-md-6 ">
                 <img src={gamificacion} className="gam" alt="gam" />
               </div>
-              <h5 className="textSolucion">
+              <div className="ctngav row ">
+          <div className="container col-sm-12 col-md-12">
+            <div className="row ">
+              <div className=" col-sm-12 col-md-6">
+                
+                <p
+                  className="comoToda mt-4
+                "
+                >
+                  
+              Por medio de la investigación y considerando los usuarios declarados anteriormente, hubieron importantes hallazgos que permitieron generar una solución efectiva. Estos hallazgos, por un lado, se relacionan directamente con los que los padres de este rango etario de niños declararon. Entre estos, <strong>la necesidad de crear hábitos, poder explicar lo que significa el virus de manera didáctica.</strong>
+            
+              </p>
+
+              
+              </div>
+
+              <div className="col-md-6">
+                
+                <p className=" entreestos ">No generar más confusión con más información y también mostrar que podría existir una salida y una mejora. Por otro lado, encontrar la manera de poder hacer que el contenido pudiera llegar a nuestros usuarios indirectos.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+              <h5 className="textSoluciongav">
                 <mark
                   style={{
                     background: "rgba(39, 146, 138, 0.49)",
@@ -361,13 +383,8 @@ class JapanQueen extends React.Component {
                   Solución al problema
                 </mark>
               </h5>
-              <p className="segun">
-                Según nuestra investigación, diferentes iteraciones y
-                entrevistas de usuario, decidimos que la mejor solución sería
-                diseñar una interfaz que funcione por medio de Tablet y
-                computador, en la que como principal objetivo, se podrá tomar
-                pedidos, eliminar y cambiar según la decisión del cliente.
-              </p>
+              
+              <p className="porotro"> La solución final fue crear una interfaz desarrollada para <strong>desktop y mobile</strong>, considerando que nuestros usuarios pueden tener acceso a un computador, sin embargo en el presente el acceso a un teléfono es más generalizado. El diseño de la página hace referencia a un tablero de juego donde el usuario puede elegir a qué sección acceder, en cada una existe material explicativo sobre el virus y su viaje por el mundo, como un cuento que puede ser relatado. </p><p className="cadaseccion">Cada sección también contiene imágenes alusivas al tema, caricaturas, audios y videos que corresponden a la serie de <strong>"Puma espuma y el escuadrón prevención"</strong>. Este material audiovisual <strong>soluciona el problema relacionado con los usuarios que poseen capacidades diferentes.</strong> Juegos y actividades para realizar en casa, además de material descargable, <strong>solucionando el problema relacionado al acceso limitado a wifi.</strong></p>
               <h5 className="resultado">
                 <mark
                   style={{
